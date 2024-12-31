26cade1bffe45c1e9d172ef91c79b3f556ce3e9a
Spencer Putt <sputt@alumni.iu.edu>
2024-12-30 22:45:21 -0800

    WIP

---
+++ private/buildozer.template.bat
@@ -0,0 +1,20 @@
+@echo off
+SETLOCAL ENABLEEXTENSIONS
+SETLOCAL ENABLEDELAYEDEXPANSION
+set MF=%RUNFILES_MANIFEST_FILE:/=\\%
+set PATH=%SYSTEMROOT%\\system32
+
+for /F "tokens=2* usebackq" %%i in (`findstr.exe /l /c:"%%BUILDOZER_RLOCATIONPATH%% " "%MF%"`) do (
+  set BUILDOZER=%%i
+  set BUILDOZER=!BUILDOZER:/=\\!
+)
+if "!BUILDOZER!" equ "" (
+  echo>&2 ERROR: %%BUILDOZER_RLOCATIONPATH%% not found in runfiles.
+  exit /b 1
+)
+
+cd %BUILD_WORKSPACE_DIRECTORY%
+!BUILDOZER! %*
+if %ERRORLEVEL% neq 0 (
+  exit /b %ERRORLEVEL%
+)
